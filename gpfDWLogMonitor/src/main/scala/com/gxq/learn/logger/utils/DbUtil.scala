0a09e5dfc6251c1acc6ba7c5726c67362f995d65
Administrator <Administrator@F1RU37WELLAEON0>
2017-10-08 12:41:12 +0800

    fix

---
+++ gpfDWLogMonitor/src/main/scala/com/gxq/learn/logger/utils/DbUtil.scala
@@ -0,0 +1,47 @@
+package com.gxq.learn.logger.utils
+
+import java.sql.{ Connection, DriverManager, Statement }
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.spark.sql.{ DataFrame, SparkSession }
+
+object DbUtil {
+  //运营数据库
+  val ADMIN_DB_URL = "jdbc:mysql://localhost:3306/tpdb?useUnicode=true&characterEncoding=UTF8"
+  val ADMIN_DB_USERNAME = "rds_admin"
+  val ADMIN_DB_PASSWORD = "changmi890*()"
+  //渠道
+  val CHANNEL_DB_URL = "jdbc:mysql://localhost:3306/channeldb?useUnicode=true&characterEncoding=UTF8"
+  val CHANNEL_DB_USERNAME = "rds_channel"
+  val CHANNEL_DB_PASSWORD = "changmi890*()"
+
+  //广告主
+  val ADV_DB_URL = "jdbc:mysql://localhost:3306/adverdb?useUnicode=true&characterEncoding=UTF8"
+  val ADV_DB_USERNAME = "rds_ad"
+  val ADV_DB_PASSWORD = "changmi890*()"
+
+  //IMEI库
+  val IMEI_DB_URL = "jdbc:mysql://localhost:3306/IMEI?useUnicode=true&characterEncoding=UTF8"
+  val IMEI_DB_USERNAME = "root"
+  val IMEI_DB_PASSWORD = "123456"
+  val IMEI_ALL_TABLE = "t_imei_all"
+
+  def cleanTable(url: String, username: String, password: String, tablename: String) = {
+    var dbconn: Connection = null
+    var stmt: Statement = null
+    try {
+      dbconn = DriverManager.getConnection(url, username, password)
+      stmt = dbconn.createStatement()
+      stmt.execute("truncate table " + tablename)
+    } catch {
+      case e: Exception =>
+        println(">>>>>>>>>>>>清空表失败")
+        e.printStackTrace()
+    } finally {
+      if (stmt != null)
+        stmt.close()
+      if (dbconn != null)
+        dbconn.close()
+    }
+  }
+}
---
+++ gpfDWLogMonitor/src/main/scala/com/gxq/learn/logger/utils/LogSortingUtil.scala
@@ -0,0 +1,15 @@
+package com.gxq.learn.logger.utils
+
+object LogSortingUtil {
+
+  /**
+   * 自定义的一个二元组的比较器
+   */
+  object TupleOrdering extends scala.math.Ordering[(String, Int)] {
+    override def compare(x: (String, Int), y: (String, Int)): Int = {
+      // 按照出现的次数进行比较，也就是按照二元组的第二个元素进行比较
+      x._2.compare(y._2)
+    }
+  }
+
+}
